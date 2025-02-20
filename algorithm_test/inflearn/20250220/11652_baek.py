fa06eaab118227f9f94d6e2bca2d01b162724c93
LeeKwanHyeong <asd95101@naver.com>
2025-02-20 17:40:48 +0900

    20250220_commit

---
+++ algorithm_test/inflearn/20250220/11652_baek.py
@@ -0,0 +1,19 @@
+import sys
+from collections import Counter
+# 준규가 가지고 있는 카드가 주어졌을 때, 가장 많이 가지고 있는 정수를 구하는 프로그램
+
+sys.stdin = open('inflearn/20250220/11652_input.txt')
+
+N = int(sys.stdin.readline().strip())
+cards = [int(sys.stdin.readline().strip()) for _ in range(N)]
+
+counter = Counter(cards)
+print(counter)
+# - max를 사용할 때 key 기준을 (빈도수, 숫자의 음수 값)으로 설정
+# - 동일한 빈도가 있다면 음수 값이 더 큰 (=원래 값이 작은) 숫자가 우선됨
+#  1) counter[x] -> 등장 빈도 기준 정렬
+#  2) -x -> 빈도가 같다면 숫자가 작은 것을 선택
+
+most_common_num = max(counter.keys(), key=lambda x: (counter[x], -x))
+
+print(most_common_num)
---
+++ algorithm_test/inflearn/20250220/1431_baek.py
@@ -0,0 +1,21 @@
+import sys
+
+# 시리얼 번호 실버3
+# 각각 기타는 모두 다른 시리얼 번호를 가지고 있다. 기타를 빨리 찾아서 
+# 빨리 사람들에게 연주해주기 위해 시리얼 번호 순서대로 정렬하고자 한다.
+# 모든 시리얼 번호는 알파벳 대문자 (A-Z)와 숫자 (0-9)로 이루어져 있다.
+
+# 1. A와 B의 길이가 다르면, 짧은 것 먼저
+# 2. 길이가 같다면 A의 모든 자리수의 합과 B의 모든 자리수 합 비교
+# 3. 만약 1, 2 번 조건으로 비교 불가능 하면 사전순으로 비교
+
+def sum_of_digits(s):
+    return sum(int(c) for c in s if c.isdigit())
+
+sys.stdin = open("inflearn/20250220/1431_input.txt")
+N = int(input())
+
+serial = [input() for _ in range(N)]
+serial.sort(key = lambda x: (len(x), sum_of_digits(x), x))
+for i in serial:
+    print(i)
