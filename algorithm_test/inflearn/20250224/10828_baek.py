e6be6a77e090355d010cd7a6d81aa8da90c8461b
LeeKwanHyeong <asd95101@naver.com>
2025-02-24 22:43:32 +0900

    20250224_commit

---
+++ algorithm_test/inflearn/20250224/10828_baek.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdin = open('inflearn/20250224/10828_input.txt')
+
+N = int(sys.stdin.readline())
+temp = []
+
+for i in range(N):
+    line = sys.stdin.readline().strip().split()
+
+    name, *value = line
+    value = value[0] if value else None
+
+    if name == 'push':
+        temp.append(value)
+    elif name == 'pop':
+        if temp:
+            a = temp.pop()
+            print(a)
+        else:
+            print(-1)
+    elif name == 'size':
+        print(len(temp))
+    elif name == 'empty':
+        if temp:
+            print(0)
+        else:
+            print(1)
+    elif name == 'top':
+        if temp:
+            print(temp[-1])
+        else:
+            print(-1)
+    else:
+        print(0)
---
+++ algorithm_test/inflearn/20250224/7795_baek.py
@@ -0,0 +1,20 @@
+import sys
+import bisect
+sys.stdin = open('inflearn/20250224/7795_input.txt')
+
+# A는 자기보다 크기가 작은 먹이만 먹을 수 있다.
+
+T = int(sys.stdin.readline())
+for case in range(T):
+    count = 0
+    N, M = map(int, sys.stdin.readline().split())
+    a_size = list(map(int, sys.stdin.readline().split()))
+    b_size = list(map(int, sys.stdin.readline().split()))
+    a_size.sort()
+    b_size.sort()
+
+    for a in a_size:
+        idx = bisect.bisect_right(b_size, a - 1)
+        count += idx
+    
+    print(count)
